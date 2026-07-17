229ad33aa1bf75aab7933771a0b03ccaadc40fba
wwxqq <1039747540@qq.com>
2026-07-17 09:27:13 +0800

    项目改造 react + vite 【】

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+import App from './app';
+
+const root = document.getElementById('root');
+if (root) {
+  const reactRoot = createRoot(root);
+  reactRoot.render(<App/>);
+}
